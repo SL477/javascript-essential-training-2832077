5ef6bd1972275bf08b3f0159ee2a13d4e8d57197
Tom Fishwick <fishwickt@gmail.com>
2022-05-30 15:10:52 +0100

    3.7

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,12 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+const jacket = {
+    color: 'black',
+    zipper: true,
+    pockets: 2,
+    contents: [{
+        name: 'phone',
+        color: 'black',
+    }],
+};
