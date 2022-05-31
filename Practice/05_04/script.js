51835a5af432969bb7ee90a3eafe1da4ecfdcc9c
Tom Fishwick <fishwickt@gmail.com>
2022-05-31 18:03:59 +0100

    5.4

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,17 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+let backpackName = document.querySelector(".backpack__name");
+console.log(backpackName.textContent);
+
+backpackName.textContent = "Posh Backpack";
+
+let bpvols = document.querySelectorAll(".backpack__volume");
+bpvols.forEach(element => {
+    console.log(element.textContent);
+});
+
+let frogPack = document.getElementById("pack02");
+
+let frogAge = frogPack.querySelector(".backpack__age");
+console.log(frogAge.textContent);
