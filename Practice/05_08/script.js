cb98a2a4b1e295df4cef4ad5398e3a8dfdd4cca0
Tom Fishwick <fishwickt@gmail.com>
2022-06-02 16:16:50 +0100

    5.8

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,25 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+let h = document.querySelector(".page-header__heading");
+
+
+h.classList.add("test");
+h.classList.add("test2");
+h.classList.remove("test");
+console.log(h);
+
+h.setAttribute("style", 'color:green');
+
+//h.attributes
+
+//console.log(h.attributes);
+let i = document.querySelector("img");
+console.log(i.getAttribute("src"));
+
+console.log('alt', i.getAttribute("alt"));
+
+i.setAttribute('alt', "An image");
+
+console.log('alt', i.getAttribute("alt"));
