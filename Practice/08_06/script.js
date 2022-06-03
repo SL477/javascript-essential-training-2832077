812ccb709a7d212d25042ef0f011ed8a2396a00c
Tom Fishwick <fishwickt@gmail.com>
2022-06-03 17:12:21 +0100

    8.6

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,24 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+function articleCreator(data) {
+    //let a = `<article>${data.text}</article>`;
+    let a = document.createElement('article');
+
+    a.innerHTML = data.text;
+    a.classList.add(data.class);
+    document.querySelector('main').appendChild(a);
+}
+
+articleCreator({text: 'hi', class: 'test'});
+
+const articleCreator2 = (data) => {
+    //let a = `<article>${data.text}</article>`;
+    let a = document.createElement('article');
+
+    a.innerHTML = data.text;
+    a.classList.add(data.class);
+    document.querySelector('main').appendChild(a);
+};
+
+articleCreator({text: 'arrow', class: 'test2'});
